72fe6bc353a61bc40a0757b4e6caf679c77a5fe1
Cuong Hoang <30700905+cuongcan2020@users.noreply.github.com>
2018-01-24 23:33:52 -0800

    Add files via upload

---
+++ T_matrix.py
@@ -0,0 +1,20 @@
+'''
+Author: Cuong Hoang
+UID: 004847900
+Date: 1/23/2018
+Usage: the T_matrix takes input from the D-H parameters and feed into
+the matrix we derived in class.
+'''
+from sympy import Matrix
+from numpy import matrix
+from sympy import sin, cos
+import numpy
+def T_matrix (alpha,a,d,theta):
+    T = Matrix ([[cos(theta), -sin(theta), 0 ,a],
+               [sin(theta)* cos (alpha), cos(theta) * cos(alpha), -sin (alpha), -sin(alpha) *d],
+               [sin(theta)*sin(alpha), cos(theta) * sin (alpha), cos(alpha), cos(alpha) *d],
+               [0,0,0,1]])
+    return T
+
+
+
